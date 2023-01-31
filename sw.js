4471e4efb5b586174693593a8091b71bdb957351
kerezsiz42 <kerezsiz42@gmail.com>
2023-01-31 22:47:17 +0000

    Updates

---
+++ sw.js
@@ -0,0 +1,13 @@
+// @ts-check
+/// <reference no-default-lib="false"/>
+/// <reference lib="ES2015" />
+/// <reference lib="webworker" />
+
+/** @type {ServiceWorkerGlobalScope} */
+// @ts-ignore
+const sw = self;
+
+sw.addEventListener("notificationclick", (e) => {
+  e.notification.close();
+  console.log(e.notification.data);
+});
